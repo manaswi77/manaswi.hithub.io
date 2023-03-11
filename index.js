5757668cef141cf24f732f9399f4f676abde854c
manaswi737 <127378313+manaswi77@users.noreply.github.com>
2023-03-11 19:36:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+//for-in loop 
+/*
+const obj = {
+  aruna: 91,
+  karan: 44,
+  aravati: 88,
+  kavya: 97,
+  bharti: 78
+}
+for(let a in obj){
+  console.log("marks of " + a + " are " + obj[a])
+}
+*/
+/*
+for(let b of "manaswi"){
+  console.log(b)
+}
+*/
+/*
+// while loop
+let a = prompt("enter the value of n")
+a = Number.parseInt(a)
+let i = 0;
+let sum = 0;
+while(i < a){
+ // alert(i)
+  //i++;
+sum += i;
+  i++;
+}
+console.log(sum)
+*/
+
+// do while loop
+/*
+console.log("hi")
+
+const avg = (a, b, c, d, e)=> {
+  return ((a + b + c + d + e)/5)
+}
+console.log(avg(87,45,55,10,88)) */
+/*
+let name = "manaswi"
+console.log(name[0])
+console.log(name.length)
+
+let sentence = `my nam'e "is" manaswi`
+console.log(sentence)
+
+let name1 = "coco"
+let a = `the name of the dog is ${name1}`
+console.log(a) */
+
+/*
+let str = 'my name is coco'
+let i = 0;
+for(i = 0; i < (str.length); i++){
+    console.log(str[i])
+}*/
+/*
+let str1 = 'please give RS 1000'
+console.log(str1.slice(15,19))*/
+
+// change the 4th char of string.
+let name = 'deepika'
+a = name.slice(3,4)
+console.log(a)
+let b = name.slice(3,4)
+b = 'v'
+console.log(b)
+console.log(name.slice(0,3) + b + name.slice(4,7))
+
